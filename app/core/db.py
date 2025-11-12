bb2f3a2cabc721d3dfe4f7590dda91867453d9b7
Weffy61 <weffy@mail.ru>
2025-11-12 22:16:10 +0300

    Update db url

--- app/core/db.py
+++ app/core/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tv.db"
+DATABASE_URL = "sqlite:////data/tv.db"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
